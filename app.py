25af973f7eecce823535a20b3b7c5a8c3add8897
YiZheHong <yhong@brandeis.edu>
2024-04-11 14:48:02 -0400

    Test

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from joblib import dump, load
-nn_clf = load('Diabetes_MLP.joblib')
+# nn_clf = load('Diabetes_MLP.joblib')
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -17,8 +17,8 @@ def index():
         user_data = [[age, hypertension, bmi, hba1c, blood_glucose]]
 
         # Make a prediction
-        prediction = nn_clf.predict(user_data)[0]
-
+        # prediction = nn_clf.predict(user_data)[0]
+        prediction = 0
         if prediction == 0:
             result = "You do not have diabetes."
         else:
