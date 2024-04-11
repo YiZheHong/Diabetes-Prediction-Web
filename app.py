fa144766ba7f8492ca864427b764091ef388160e
YiZheHong <62890768+YiZheHong@users.noreply.github.com>
2024-04-11 08:57:01 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+from sklearn.neural_network import MLPClassifier
+import numpy as np
+from joblib import dump, load
+# from ModelPrep import nn_clf
+nn_clf = load('Diabetes_MLP.joblib')
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Get user input data
+        age = float(request.form['age'])
+        bmi = float(request.form['bmi'])
+        hba1c = float(request.form['hba1c'])
+        blood_glucose = float(request.form['blood_glucose'])
+        hypertension = float(request.form['hypertension'])
+
+        # Create a sample data point
+        user_data = np.array([[age, hypertension, bmi, hba1c, blood_glucose]])
+
+        # Make a prediction
+        prediction = nn_clf.predict(user_data)[0]
+
+        if prediction == 0:
+            result = "You do not have diabetes."
+        else:
+            result = "You have diabetes."
+
+        return render_template('index1.html', result=result)
+
+    return render_template('index1.html')
+
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
